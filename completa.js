93991bba1156dd4a24187cce36f7e7a7b2d4e5ca
Raúl <raul.es0407@gmail.com>
2021-07-18 15:44:47 +0200

    Create completa.js

---
+++ completa.js
@@ -0,0 +1,251 @@
+Swal.fire({
+  title: '¡Bienvenido a <b>Completa</b>!',
+  text: 'Rellena todos los espacios con su correspondiente elemento.  Si está bien, felicidades; de lo contrario, podrás repetirlo y comprobar otra vez.',
+  footer: 'Puedes resetear tus respuestas con el botón que se encuentra en la parte inferior.',
+  confirmButtonText: 'Entendido',
+  denyButtonText: 'Cancelar',
+  timer: 20000,
+  timerProgressBar: true,
+});
+
+function verificar() {
+    fn001();
+    fn002();
+    fn003();
+    fn004();
+    fn005();
+    fn006();
+    fn007();
+    fn008();
+    fn009();
+    fn010();
+    fn011();
+    fn012();
+    fn013();
+    fn014();
+    fn015();
+    fn016();
+    fn017();
+    fn018();
+}
+
+function reset() {
+    window.location.reload();
+}
+
+  
+function fn001() {
+var str1 = document.getElementById("H").value;
+  if (str1 == 'H') {
+    document.getElementById("H").style.border="3px #77dd77 solid";
+    document.getElementById("H").disabled = true;
+  }
+  else {
+    document.getElementById("H").style.border="3px #de1738 solid";
+    document.getElementById("H").disabled = false;
+  }
+}
+  
+function fn002() {
+var str2 = document.getElementById("He").value;
+  if (str2 == 'He') {
+    document.getElementById("He").style.border="3px #77dd77 solid";
+    document.getElementById("He").disabled = true;
+  }
+  else {
+    document.getElementById("He").style.border="3px #de1738 solid";
+    document.getElementById("He").disabled = false;
+  }
+}
+
+function fn003() {
+var str3 = document.getElementById("Li").value;
+  if (str3 == 'Li') {
+    document.getElementById("Li").style.border="3px #77dd77 solid";
+    document.getElementById("Li").disabled = true;
+  }
+  else {
+    document.getElementById("Li").style.border="3px #de1738 solid";
+    document.getElementById("Li").disabd = false;
+  }
+}
+
+function fn004() {
+var str4 = document.getElementById("Be").value;
+  if (str4 == 'Be') {
+    document.getElementById("Be").style.border="3px #77dd77 solid";
+    document.getElementById("Be").disabled = true;
+  }
+  else {
+    document.getElementById("Be").style.border="3px #de1738 solid";
+    document.getElementById("Be").disabled = false;
+  }
+}
+
+function fn005() {
+var str5 = document.getElementById("B").value;
+  if (str5 == 'B') {
+    document.getElementById("B").style.border="3px #77dd77 solid";
+    document.getElementById("B").disabled = true;
+  }
+  else {
+    document.getElementById("B").style.border="3px #de1738 solid";
+    document.getElementById("B").disabled = false;
+  }
+}
+
+function fn006() {
+var str6 = document.getElementById("C").value;
+  if (str6 == 'C') {
+    document.getElementById("C").style.border="3px #77dd77 solid";
+    document.getElementById("C").disabled = true;
+  }
+  else {
+    document.getElementById("C").style.border="3px #de1738 solid";
+    document.getElementById("C").disabled = false;
+  }
+}
+
+function fn007() {
+var str7 = document.getElementById("N").value;
+  if (str7 == 'N') {
+    document.getElementById("N").style.border="3px #77dd77 solid";
+    document.getElementById("N").disabled = true;
+  }
+  else {
+    document.getElementById("N").style.border="3px #de1738 solid";
+    document.getElementById("N").disabled = false;
+  }
+} 
+
+function fn008() {
+var str8 = document.getElementById("O").value;
+  if (str8 == 'O') {
+    document.getElementById("O").style.border="3px #77dd77 solid";
+    document.getElementById("O").disabled = true;
+  }
+  else {
+    document.getElementById("O").style.border="3px #de1738 solid";
+    document.getElementById("O").disabled = false;
+  }
+} 
+
+function fn009() {
+var str9 = document.getElementById("F").value;
+  if (str9 == 'F') {
+    document.getElementById("F").style.border="3px #77dd77 solid";
+    document.getElementById("F").disabled = true;
+  }
+  else {
+    document.getElementById("F").style.border="3px #de1738 solid";
+    document.getElementById("F").disabled = false;
+  }
+} 
+
+function fn010() {
+var str10 = document.getElementById("Ne").value;
+  if (str10 == 'Ne') {
+    document.getElementById("Ne").style.border="3px #77dd77 solid";
+    document.getElementById("Ne").disabled = true;
+  }
+  else {
+    document.getElementById("Ne").style.border="3px #de1738 solid";
+    document.getElementById("Ne").disabled = false;
+  }
+}
+
+function fn011() {
+var str11 = document.getElementById("Na").value;
+  if (str11 == 'Na') {
+    document.getElementById("Na").style.border="3px #77dd77 solid";
+    document.getElementById("Na").disabled = true;
+  }
+  else {
+    document.getElementById("Na").style.border="3px #de1738 solid";
+    document.getElementById("Na").disabled = false;
+  }
+}
+
+function fn012() {
+var str12 = document.getElementById("Mg").value;
+  if (str12 == 'Mg') {
+    document.getElementById("Mg").style.border="3px #77dd77 solid";
+    document.getElementById("Mg").disabled = true;
+  }
+  else {
+    document.getElementById("Mg").style.border="3px #de1738 solid";
+    document.getElementById("Mg").disabled = false;
+  }
+}
+
+function fn013() {
+var str13 = document.getElementById("Al").value;
+  if (str13 == 'Al') {
+    document.getElementById("Al").style.border="3px #77dd77 solid";
+    document.getElementById("Al").disabled = true;
+  }
+  else {
+    document.getElementById("Al").style.border="3px #de1738 solid";
+    document.getElementById("Al").disabled = false;
+  }
+}
+
+function fn014() {
+var str14 = document.getElementById("Si").value;
+  if (str14 == 'Si') {
+    document.getElementById("Si").style.border="3px #77dd77 solid";
+    document.getElementById("Si").disabled = true;
+  }
+  else {
+    document.getElementById("Si").style.border="3px #de1738 solid";
+    document.getElementById("Si").disabled = false;
+  }
+}
+
+function fn015() {
+var str15 = document.getElementById("P").value;
+  if (str15 == 'P') {
+    document.getElementById("P").style.border="3px #77dd77 solid";
+    document.getElementById("P").disabled = true;
+  }
+  else {
+    document.getElementById("P").style.border="3px #de1738 solid";
+    document.getElementById("P").disabled = false;
+  }
+}
+
+function fn016() {
+var str16 = document.getElementById("S").value;
+  if (str16 == 'S') {
+    document.getElementById("S").style.border="3px #77dd77 solid";
+    document.getElementById("S").disabled = true;
+  }
+  else {
+    document.getElementById("S").style.border="3px #de1738 solid";
+    document.getElementById("S").disabled = false;
+  }
+}
+
+function fn017() {
+var str17 = document.getElementById("Cl").value;
+  if (str17 == 'Cl') {
+    document.getElementById("Cl").style.border="3px #77dd77 solid";
+    document.getElementById("Cl").disabled = true;
+  }
+  else {
+    document.getElementById("Cl").style.border="3px #de1738 solid";
+    document.getElementById("Cl").disabled = false;
+  }
+}
+
+function fn018() {
+var str18 = document.getElementById("Ar").value;
+  if (str18 == 'Ar') {
+    document.getElementById("Ar").style.border="3px #77dd77 solid";
+    document.getElementById("Ar").disabled = true;
+  }
+  else {
+    document.getElementById("Ar").style.border="3px #de1738 solid";
+    document.getElementById("Ar").disabled = false;
+  }
+}
