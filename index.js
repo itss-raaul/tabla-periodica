0a90c481e9d6225ab984d426fde385260cb34a6e
Raúl <raul.es0407@gmail.com>
2021-08-25 14:38:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ Swal.fire({
   title: 'Selecciona la dificultad',
   icon: 'question',
   showDenyButton: true,
-  confirmButtonText: '<a id="medio" href="https://000625786.codepen.website/completamedio.html">Medio</a> <style> a {color: white; text-decoration: none;} .medio {background-color: yellow;} </style>',
-  denyButtonText: '<a id="dificil" href="https://000625786.codepen.website/completavanzado.html">Dificil</a> <style> a {color: white; text-decoration: none;} .dificil {background-color: red;}</style>',
+  confirmButtonText: '<a id="medio" href="https://tabla-periodica-game.netlify.app/completamedio.html">Medio</a> <style> a {color: white; text-decoration: none;} .medio {background-color: yellow;} </style>',
+  denyButtonText: '<a id="dificil" href="https://tabla-periodica-game.netlify.app/completavanzado.html">Dificil</a> <style> a {color: white; text-decoration: none;} .dificil {background-color: red;}</style>',
   html:
-    '<a class="facil" href="https://000625786.codepen.website/completa.html">Fácil</a> <style> .facil {background-color: green; font: 400 13.3333px Arial; display: inline-block; margin: .3125em; padding: .625em 1.1em; transition: box-shadow .1s; box-shadow: 0 0 0 3px transparent; font-weight: 500; cursor: pointer; border: 0; border-radius: .25em; background: initial; background-color: grey; color: #fff; font-size: 1em; appearance: auto; -webkit-writing-mode: horizontal-tb !important; text-rendering: auto; letter-spacing: normal; word-spacing: normal; text-transform: none; text-indent: 0px; text-shadow: none; text-align: center; align-items: flex-start; box-sizing: border-box;} </style>',
+    '<a class="facil" href="https://tabla-periodica-game.netlify.app/completa.html">Fácil</a> <style> .facil {background-color: green; font: 400 13.3333px Arial; display: inline-block; margin: .3125em; padding: .625em 1.1em; transition: box-shadow .1s; box-shadow: 0 0 0 3px transparent; font-weight: 500; cursor: pointer; border: 0; border-radius: .25em; background: initial; background-color: grey; color: #fff; font-size: 1em; appearance: auto; -webkit-writing-mode: horizontal-tb !important; text-rendering: auto; letter-spacing: normal; word-spacing: normal; text-transform: none; text-indent: 0px; text-shadow: none; text-align: center; align-items: flex-start; box-sizing: border-box;} </style>',
 })
