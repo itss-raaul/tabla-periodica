44d7dfac00419650d24f1fe43c7de83a40b67a7a
Raúl <raul.es0407@gmail.com>
2021-09-10 23:12:33 +0200

    Update adivinaelelemento.js

--- adivinaelelemento.js
+++ adivinaelelemento.js
@@ -5,14 +5,7 @@ Swal.fire({
   confirmButtonText: 'Adelante',
   timer: 10000,
   timerProgressBar: true,
-}).then(function () {
-  const Toast = Swal.mixin({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 7000,
-  timerProgressBar: true,
-});
+})
 
 // Hidrógeno
 document.getElementById("H").onclick = function() {myFunction1()};
