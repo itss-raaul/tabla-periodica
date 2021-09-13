72a787b9529a40b0bc386875ff5086c8c3b88447
Raúl <raul.es0407@gmail.com>
2021-09-13 13:21:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('install',function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
       return cache.add('index.html')
