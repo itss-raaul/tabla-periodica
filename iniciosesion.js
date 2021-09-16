9a449c981b19b19fd75b65b3d6202a96bf59331f
Raúl <raul.es0407@gmail.com>
2021-09-16 16:46:44 +0200

    Update iniciosesion.js

--- iniciosesion.js
+++ iniciosesion.js
@@ -1,29 +1,3 @@
-
-
-if (usuario === "raul" && password === "raul") {
- Swal.fire({
-    title: 'Has iniciado sesión correctamente!',
-    icon: 'success',
-    confirmButtonText: 'Continuar'
-  });
-   window.location="/index.html";
-}
-    
-else {
-  Swal.fire({
-    title: 'Tu usuario/contraseña no es correcta',
-    text: "Inténtalo de nuevo",
-    icon: 'error',
-    confirmButtonText: 'Volver a intentar'
-  });
-  window.location="/index.html"
-
-}
-
-
-})(); }
-
-
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
