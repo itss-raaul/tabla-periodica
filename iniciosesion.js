26a5f8686023f582e7c715a832394a3e0404a7b0
Raúl <raul.es0407@gmail.com>
2021-09-16 16:59:32 +0200

    Update iniciosesion.js

--- iniciosesion.js
+++ iniciosesion.js
@@ -1,4 +1,3 @@
-var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
 var username = document.getElementById("username").value;
@@ -6,16 +5,5 @@ var password = document.getElementById("password").value;
 if ( username == "raul.es0407@gmail.com" && password == "raul21042007"){
 alert ("Login successfully");
 window.location = "/index.html"; // Redirecting to other page.
-}
-else{
-attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
 }
 }
