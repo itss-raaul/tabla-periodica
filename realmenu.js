f9bc4ee7c142f3f9947ba209ece53554e20679d6
Raúl <raul.es0407@gmail.com>
2021-09-17 13:29:56 +0200

    Update realmenu.js

--- realmenu.js
+++ realmenu.js
@@ -1,18 +1,5 @@
-(function () {
-  const Toast = Swal.mixin({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 7000,
-  timerProgressBar: true,
-})
-
 window.addEventListener('load', () =>  {
   registerSW();
-  Toast.fire({
-    icon: 'warning',
-    title: 'Estás en la versión beta'
-});
 });
 
  async function registerSW() {
