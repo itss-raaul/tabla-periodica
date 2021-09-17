ffa8828bedcd35ad683bd567d0582dde0f931a37
Raúl <raul.es0407@gmail.com>
2021-09-17 12:40:13 +0200

    Update iniciosesion.js

--- iniciosesion.js
+++ iniciosesion.js
@@ -3,18 +3,11 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "raul@gmail.com" && password == "12345"){
-const Toast = Swal.mixin({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 7000,
-  timerProgressBar: true,
-})
-
-Toast.fire({
-  icon: 'success',
-  title: 'Loggin Successfully!'
-})
+Swal.fire(
+  'Good job!',
+  'You clicked the button!',
+  'success'
+)
 
 });
 window.location = "/realmenu.html"; // Redirecting to other page.
