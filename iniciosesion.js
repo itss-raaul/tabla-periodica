75bb4a2e19b097af688375f5de0349b4fb0eaa03
Raúl <raul.es0407@gmail.com>
2021-09-17 17:16:06 +0200

    Update iniciosesion.js

--- iniciosesion.js
+++ iniciosesion.js
@@ -5,3 +5,11 @@ if ( username == "raul@gmail.com" && password == "12345"){
    window.location = "/realmenu.html"; // Redirecting to other page.
 };
 }
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
