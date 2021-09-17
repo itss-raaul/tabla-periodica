06303e3d77dc0b332d52a43cd03104f7a054415d
Raúl <raul.es0407@gmail.com>
2021-09-17 13:17:56 +0200

    Update iniciosesion.js

--- iniciosesion.js
+++ iniciosesion.js
@@ -11,4 +11,4 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "raul@gmail.com" && password == "12345"){
    window.location = "/realmenu.html"; // Redirecting to other page.
-}:
+};
