20c4dfb4c01fdcdee6e0908dc9edb3113df03f3b
Raúl <raul.es0407@gmail.com>
2021-09-17 11:53:25 +0200

    Update iniciosesion.js

--- iniciosesion.js
+++ iniciosesion.js
@@ -3,7 +3,20 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "raul@gmail.com" && password == "12345"){
-alert ("Login successfully");
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 7000,
+  timerProgressBar: true,
+})
+
+Toast.fire({
+  icon: 'success',
+  title: 'Loggin Successfully!'
+})
+
+});
 window.location = "/realmenu.html"; // Redirecting to other page.
 }
 }
