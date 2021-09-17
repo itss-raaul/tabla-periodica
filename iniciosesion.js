37e29ed721ba6d87a4d1fd223b6a463b7e117f2d
Raúl <raul.es0407@gmail.com>
2021-09-17 12:42:38 +0200

    Update iniciosesion.js

--- iniciosesion.js
+++ iniciosesion.js
@@ -1,15 +1,13 @@
-// Below function Executes on click of login button.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "raul@gmail.com" && password == "12345"){
 Swal.fire(
-  'Good job!',
-  'You clicked the button!',
+  'Has iniciado sesión',
+  'Continua para acceder al contenido!',
   'success'
 )
 
-});
+};
 window.location = "/realmenu.html"; // Redirecting to other page.
-}
 }
