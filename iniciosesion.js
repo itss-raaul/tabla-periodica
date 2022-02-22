f30c862ab08a3b0d43b82d2dd0d3d71bf0e863ac
Raúl <raul.es0407@gmail.com>
2022-02-22 22:09:44 +0100

    Delete iniciosesion.js

--- iniciosesion.js
+++
@@ -1,7 +0,0 @@
-function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "raul@gmail.com" && password == "12345"){
-   window.location = "/realmenu.html"; // Redirecting to other page.
-};
-}
